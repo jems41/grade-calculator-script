197bfd70f02ca1d7aec4defac9bb79e04b0fed7a
James Ferdinand Combista <james.combista@dawsoncollege.qc.ca>
2024-09-27 11:10:27 -0400

    last edit to fix the number of labs/quizzes

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# user inputs notes
+# user inputs grades
 
 labprogrammingproblems = int(input("Enter the number of labs completed: "))
 quizzes = int(input("Enter the number of quizzes completed: "))
@@ -23,22 +23,27 @@ weight6 = 6
 # calculator1 for labs and quizzes (the ones out of /6)
 
 def calculator1(grade: int, weight: int):
-    return grade/6 * weight
+    if grade > 6:
+        return weight
+    else:
+        return grade/6 * weight
 
-# calculator2 for 4 assignments (the ones out of /100)
+# calculator2 for 4 assignments (4 assignment grades out of /100)
 
 def calculator2(grade1: int, grade2: int, grade3: int, grade4: int, weight: int):
     return (grade1 + grade2 + grade3 + grade4)/400 * weight
 
-# calculator3 for 2 midterms (the ones out of /100)
+# calculator3 for 2 midterms (2 midterms grades out of /100)
 
 def calculator3(grade1: int, grade2: int, weight: int):
     return (grade1 + grade2)/200 * weight
 
-# calculator4 for final, midterm and final preparation (the ones out of /100)
+# calculator4 for final, midterm and final preparation (out grade out of /100)
 
 def calculator4(grade: int, weight: int):
     return grade/100 * weight
+
+# inputs the variables to the functions
 
 result1 = calculator1(labprogrammingproblems, weight1)
 result2 = calculator1(quizzes, weight2)
@@ -47,6 +52,10 @@ result4 = calculator3(midterm1, midterm2, weight4)
 result5 = calculator4(final, weight5)
 result6 = calculator4(midterms, weight6)
 
+# calculates the sum of all the results
+
+final_result = (int(result1 + result2 + result3 + result4 + result5 + result6))
+
 # prints the final grade
 
-print(int(result1 + result2 + result3 + result4 + result5 + result6))
+print("Your grade is: " + str(final_result))
